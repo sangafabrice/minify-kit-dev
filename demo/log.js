64f889e9493701adc5e5fd31e56266aaeda002af
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2025-12-17 01:52:44 +0100

    test(demo): remove module resolution error suppressions

--- demo/log.js
+++ demo/log.js
@@ -1,9 +1,5 @@
-/** @flow */
-// $FlowExpectedError[missing-this-annot]
-// $FlowExpectedError[missing-local-annot]
 function log (extname) {
     this.then(console.log.bind(console, `[${extname}]:`.padEnd(8, " ")));
 }
 
-// $FlowExpectedError[cannot-resolve-name]
 Promise.prototype.log = log;
