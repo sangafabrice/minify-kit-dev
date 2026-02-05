aa9916a1127fda8bb5a3b0873f93e5dc895d4531
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2026-02-05 10:12:40 +0100

    chore: modify to linting with node bin

---
+++ build/lint.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+/*jslint-disable*/
+import fs from "node:fs/promises";
+import jslint from "@jslint-org/jslint";
+
+async function main(files) {
+    for await (const file of files) {
+        fs.readFile(file, "utf8")
+            .then(jslint)
+            .then(({ warnings }) => {
+                console.log("jslint %s", file);
+                for (const { line, column, message } of warnings) {
+                    console.log(" %d:%d %s", line, column, message);
+                }
+            })
+            .catch(console.error);
+    }
+}
+
+main(fs.glob("**/*.{js,mjs,cjs,json}", {
+    exclude: [
+        "node_modules/**",
+        ".vscode/**",
+        "flow-typed/**",
+        "**/package-lock.json"
+    ]
+}));
+/*jslint-enable*/
